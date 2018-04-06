5c554d91425b152c8928c65075bc2324319a5940
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-06 17:54:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,8 +10,12 @@ end
 
 def sort_array_char_count(array)
   return array.sort do |a, b|
-    0 if a.length == b.length
-    1 if a.length < b.length
-    -1 if a.length > b.length
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
   end
 end
