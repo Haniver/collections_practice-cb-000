b56541e56a9a06908d80e99af38985517d5dd548
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-06 17:52:43 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,7 +11,7 @@ end
 def sort_array_char_count(array)
   return array.sort do |a, b|
     0 if a.length == b.length
-    -1 if a.length < b.length
-    1 if a.length > b.length
+    1 if a.length < b.length
+    -1 if a.length > b.length
   end
 end
